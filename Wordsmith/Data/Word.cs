e05f0544a9f0d4d493645e292306a1d325859b5a
LadyDefile <67084868+LadyDefile@users.noreply.github.com>
2022-03-16 15:24:34 -0500

    Changelog # Data.Word.cs * Created File

---
+++ Wordsmith/Data/Word.cs
@@ -0,0 +1,32 @@
+﻿namespace Wordsmith.Data;
+
+internal sealed class Word
+{
+    /// <summary>
+    /// The start of the entire text segment.
+    /// </summary>
+    internal int StartIndex = -1;
+
+    /// <summary>
+    /// The index where the word starts. This can be different
+    /// from StartIndex when the text starts with punctuation.
+    /// </summary>
+    internal int WordIndex = -1;
+
+    /// <summary>
+    /// The length of the word excluding any punctuation marks.
+    /// </summary>
+    internal int WordLength = -1;
+
+    /// <summary>
+    /// The last index of the text. This can be offset from
+    /// StartIndex+WordLength if there is punctuation in the
+    /// text.
+    /// </summary>
+    internal int EndIndex = -1;
+
+    /// <summary>
+    /// A value indicating whether or not the word is in the dictionary.
+    /// </summary>
+    internal bool InDictionary = true;
+}
