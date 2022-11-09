7824aa92eee22b9f55e48f2aab71e90d396ab565
LadyDefile <MistressDefile@gmail.com>
2022-11-09 15:45:31 -0600

    # Changelog ## Data\ ### HeaderData.cs * Moved to Structs.cs
    ### Rect2.cs
    * Moved to Structs.cs
    
    ### Structs.cs
    * Created file

--- Wordsmith/Data/Structs.cs
+++ Wordsmith/Data/Structs.cs
@@ -142,3 +142,22 @@ internal struct HeaderData
         }
     }
 }
+
+internal struct Rect2
+{
+    internal Vector2 Position;
+    internal Vector2 Size;
+    public Rect2( float x, float y, float w, float h )
+    {
+        this.Position = new( x, y );
+        this.Size = new( w, h );
+    }
+    public Rect2( Vector2 position, Vector2 size )
+    {
+        this.Position = position;
+        this.Size = size;
+    }
+
+    internal bool Contains( Vector2 v ) => v.X > this.Position.X && v.X < this.Position.X + this.Size.X && v.Y > this.Position.Y && v.Y < this.Position.Y + this.Size.Y;
+
+}
