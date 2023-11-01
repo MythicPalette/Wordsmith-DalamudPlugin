ce958db6cfcda4e4e73f75651daf3f8f97df08ae
LadyDefile <MistressDefile@gmail.com>
2023-11-01 08:57:01 -0500

    * Created a statistics tracker class

---
+++ Wordsmith/Helpers/StatisticsTracker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordsmith.Helpers;
+internal class StatisticsTracker
+{
+    private Dictionary<string, int> _word_usage_count = new();
+    internal void TallyWords( string line, Word[] words)
+    {
+        foreach (Word word in words)
+        {
+            string w = line[word.WordIndex..word.WordLength];
+            if ( !this._word_usage_count.ContainsKey( w.ToLower() ) )
+                this._word_usage_count[w.ToLower()] = 1;
+            else
+                this._word_usage_count[w.ToLower()]++;
+        }
+    }
+}
